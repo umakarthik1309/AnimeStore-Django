0924a50515ca5000b225807aa1eee25d992863cc
Uma Karthik <umakarthik1309@gmail.com>
2026-07-22 18:54:55 +0530

    django authentication

--- NarutoStore/settings.py
+++ NarutoStore/settings.py
@@ -134,3 +134,18 @@ STATICFILES_DIRS = [
  
 MEDIA_URL = '/media/'
 MEDIA_ROOT = BASE_DIR /'media'
+
+
+from django.contrib.messages import constants as messages
+MESSAGE_TAGS = {
+    messages.ERROR: "danger",
+}
+
+# SMTP configuration
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = 587
+EMAIL_HOST_USER = 'eumakarthik1431996@gmail.com'
+EMAIL_HOST_PASSWORD = 'ltrp ogwq mbse syvv'
+EMAIL_USE_TLS = True
+
+# ltrp ogwq mbse syvv
--- static/js/script.js
+++ static/js/script.js
@@ -58,3 +58,7 @@ $(document).ready(function() {
 }); 
 // jquery end
 
+
+
+
+
